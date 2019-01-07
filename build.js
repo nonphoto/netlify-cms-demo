f7e101cb9167b8f4584e09c1dec6ffa4bd051cc7
Jonas Luebbers <jluebbers@squarespace.com>
2019-01-07 13:57:49 -0500

    Update build to write out HTML

--- build.js
+++ build.js
@@ -2,22 +2,47 @@
 
 const fs = require('fs')
 const YAML = require('yaml')
-const markdown = require('markdown')
+const markdown = require('markdown').markdown
 
 function template(content) {
+    const items = content.map((item) => {
+        return `
+            <div>
+                <h1>${item.title}</h1>
+                ${item.body}
+            </div>
+        `
+    }).join('')
+
     return `
         <html>
             <head>
                 <title>Netlify CMS Demo</title>
             </head>
             <body>
-                ${content}
+                ${items}
             </body>
         </html>
     `
 }
 
 const files = process.argv.slice(2)
-files.forEach((file) => {
-    fs.createReadStream(file).pipe(process.stdout)
+const promises = files.map((file) => {
+    return new Promise((resolve) => {
+        fs.readFile(file, (error, data) => {
+            if (error) throw error
+            resolve(data)
+        })
+    })
+})
+
+Promise.all(promises).then((chunks) => {
+    const content = chunks.map((chunk) => {
+        const parsedChunk = chunk.toString('utf8').match(/---((.|\n)*)---((.|\n)*)/m)
+        const data = YAML.parse(parsedChunk[1])
+        data.body = markdown.toHTML(parsedChunk[3])
+        return data
+    })
+
+    process.stdout.write(template(content))
 })
--- content/blog/2020-01-04-this-is-my-second-post.md
+++ content/blog/2020-01-04-this-is-my-second-post.md
@@ -1,5 +1,5 @@
 ---
-title: This is my first post
+title: This is my second post
 date: 2019-01-04T17:21:52.394Z
 thumbnail: /images/drtuglzv4aam5-3.jpg
 ---
